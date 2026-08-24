a629192acbcb519ed1621df8ea9e83a2ee8a9a3f
juliabardella <juliabardella3@gmail.com>
2026-08-24 16:13:06 -0300

    meu primeiro codigo

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
->>>>>>> d1624d635566a07981af995c9e4cd5bd7cb971c1
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: '/projeto-api/',
-=======
-  base: '/useHooks/'
->>>>>>> d1624d635566a07981af995c9e4cd5bd7cb971c1
+  base: '/projeto-api/'
 })
